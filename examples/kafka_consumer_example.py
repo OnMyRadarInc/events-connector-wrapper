c06c8c955fe1cd7248411fe52fdd7f1fecf45060
Sebastian <ingesql1992@hotmail.com>
2020-08-25 11:43:32 -0500

    Added base configuration

---
+++ examples/kafka_consumer_example.py
@@ -0,0 +1,19 @@
+import time
+from event_utils.event_utils import EventConsumer
+
+config= {
+    'CLOUDKARAFKA_BROKERS': '{brokers list separated by commas}',
+    'CLOUDKARAFKA_PASSWORD': '{password}',
+    'CLOUDKARAFKA_USERNAME': '{username}'
+}
+
+kafka_consumer = EventConsumer('kafka', config).create_consumer()
+
+topics = ['introduction_api']
+kafka_consumer.assign_topic(topics)
+
+while True:
+    msg = kafka_consumer.get_message(timeout=5.0)
+    if msg:
+        print(msg.value())
+    time.sleep(5)
